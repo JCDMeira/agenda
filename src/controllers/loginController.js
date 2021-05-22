af5101558b6a677214b07015b871372dbb40ce4f
JCDMeira <jean.meira10@hotmail.com>
2021-05-22 16:17:55 -0300

    Atualiza login e logout de usuários, previnindo a tentativa de cadastro enquanto logado

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,7 +1,7 @@
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res)=>{
-    
+    if(req.session.user) return res.render('login-logado')
     res.render('login');
 };
 exports.register = async (req, res)=>{
