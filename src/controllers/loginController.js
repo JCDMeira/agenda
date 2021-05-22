f753be8cc88912a26788e5ccc7b5326e009d41a7
JCDMeira <jean.meira10@hotmail.com>
2021-05-21 21:20:09 -0300

    Atualiza validação para criar conta

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -4,16 +4,25 @@ exports.index = (req, res)=>{
     res.render('login');
 };
 exports.register = async (req, res)=>{
-    const login = new Login(req.body);
-    await login.register();
-
-    if(login.errors.length > 0){
-        req.flash('errors', login.errors);
+    
+    try{
+        const login = new Login(req.body);
+        await login.register();
+    
+        if(login.errors.length > 0){
+            req.flash('errors', login.errors);
+            req.session.save(function(){
+                return res.redirect('back');
+            });
+            return
+        }
+    
+        req.flash('succes', 'Seu usuário foi criado com sucesso');
         req.session.save(function(){
             return res.redirect('back');
         });
-        return
+    }catch(e){
+        console.log(e);
+        return res.render('404')
     }
-
-    res.send(login.errors);
 };
